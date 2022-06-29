4ee2bda34824cff6f4194e1ed918eef18b61adae
Kaisy <rutyo8@mail.ru>
2022-06-29 14:52:00 +1100

    polish-not

--- src/logic/polish-notation.js
+++ src/logic/polish-notation.js
@@ -1,5 +1,68 @@
 function polishNotation(strExpr) {
-    
+    const stack = []
+    let result = ''
+    for(let i = 0; i < strExpr.length; ++i) {
+        switch(strExpr[i]) {
+            case '+': {
+                if (
+                    stack[stack.length - 1] === '-' || 
+                    stack[stack.length - 1] === '+' ||
+                    stack[stack.length - 1] === '*' ||
+                    stack[stack.length - 1] === '/'
+                ) {
+                    result += stack.pop()
+                } 
+                stack.push('+')  
+                break
+            }
+            case '-': {
+                if (
+                    stack[stack.length - 1] === '-' || 
+                    stack[stack.length - 1] === '+' ||
+                    stack[stack.length - 1] === '*' ||
+                    stack[stack.length - 1] === '/'
+                ) {
+                    result += stack.pop()
+                } 
+                stack.push('-')  
+                break
+            }
+            case '*': {
+                if (stack[stack.length - 1] === '/' || stack[stack.length - 1] === '*') {
+                    result += stack.pop()
+                }
+                stack.push('*')  
+                break
+            }
+            case '/': {
+                if (stack[stack.length - 1] === '/' || stack[stack.length - 1] === '*') {
+                    result += stack.pop()
+                } 
+                stack.push('/')  
+                break
+            }
+            case '(': {
+                stack.push('(')
+                break
+            }
+            case ')': {
+                while(stack[stack.length - 1] !== '(') {
+                    result += stack.pop()
+                }
+                stack.pop()
+                break
+            }
+            default: {
+                result += strExpr[i]
+                break
+            }
+        }
+    }
+    for(; stack.length;) {
+        result += stack.pop()
+    }
+    return result
 }
+
 
 export default polishNotation
