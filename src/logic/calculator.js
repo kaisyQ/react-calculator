82addc5e01523d7b61b7308f82b0a204a1441222
Kaisy <rutyo8@mail.ru>
2022-06-29 15:02:46 +1100

    calc-func

---
+++ src/logic/calculator.js
@@ -0,0 +1,32 @@
+function calculateRPN(RPNStr) {
+    const stack = []
+
+    for(let i = 0; i < RPNStr.length; ++i) {
+        switch(RPNStr[i]) {
+            case '+': {
+                stack.push(parseInt(stack.pop()) + parseInt(stack.pop()))
+                break
+            }
+            case '-': {
+                stack.push(-(parseInt(stack.pop()) - parseInt(stack.pop())))
+                break
+            }
+            case '*': {
+                stack.push(parseInt(stack.pop()) * parseInt(stack.pop()))
+                break
+            }
+            case '/': {
+                stack.push(1 / (parseInt(stack.pop()) / parseInt(stack.pop())))
+                break
+            }
+            default: {
+                stack.push(parseInt(RPNStr[i]))
+                console.log(stack)
+                break
+            }
+        }
+    }
+    return stack.pop()
+}
+
+console.log(calculateRPN('55/2*1-'))
